38652f8cef85c985c0001831a330cd1fb244345f
kei-kusanagi <kei_kusanagi@outlook.com>
2022-07-01 13:41:20 -0500

    mark task as done

--- django_tailwind_todo/django_tailwind_todo/urls.py
+++ django_tailwind_todo/django_tailwind_todo/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from todo.views import todos, add_todo
+from todo.views import todos, add_todo, update_todo
 
 urlpatterns = [
     path('', todos, name='todos'),
     path('add-todo/', add_todo, name='add_todo'),
+    path('update/<int:pk>/', update_todo, name='update_todo'),
     path('admin/', admin.site.urls),
 ]
