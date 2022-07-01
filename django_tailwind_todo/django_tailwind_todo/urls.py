d4f00fccd441fc2a4c01cde0cf478a7274e8f241
kei-kusanagi <kei_kusanagi@outlook.com>
2022-06-30 21:28:12 -0500

    create databasemodel

--- django_tailwind_todo/django_tailwind_todo/urls.py
+++ django_tailwind_todo/django_tailwind_todo/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from todo.views import todos
+from todo.views import todos, add_todo
 
 urlpatterns = [
     path('', todos, name='todos'),
+    path('add-todo/', add_todo, name='add_todo'),
     path('admin/', admin.site.urls),
 ]
--- django_tailwind_todo/todo/views.py
+++ django_tailwind_todo/todo/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
+
+from .models import Todo
 
 def todos(request):
     return render(request, 'todo/todos.html')
+
+@require_http_methods(['POST'])
+def add_todo(reques):
+    todo = None
+    title = request.POST.get('title', '')
+
+    if title:
+        todo = Todo.objects.creat(title=title)
+
+    return render(request, 'todo/partials/todo.html', {'todo': todo})
