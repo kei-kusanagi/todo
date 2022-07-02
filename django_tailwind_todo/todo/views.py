50cc66a328ab07159828f162535c1ed0d67ed824
kei-kusanagi <kei_kusanagi@outlook.com>
2022-07-01 20:13:06 -0500

    Delete a task

--- django_tailwind_todo/todo/views.py
+++ django_tailwind_todo/todo/views.py
@@ -1,3 +1,4 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 
@@ -22,4 +23,12 @@ def update_todo(request, pk):
     todo = Todo.objects.get(pk=pk)
     todo.is_done = True
     todo.save()
+    
     return render(request, 'todo/partials/todo.html', {'todo': todo})
+
+@require_http_methods('DELETE')
+def delete_todo(request, pk):
+    todo = Todo.objects.get(pk=pk)
+    todo.delete()
+
+    return HttpResponse()
