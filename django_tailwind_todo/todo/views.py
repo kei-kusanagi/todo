16fb31e721cfcea85594d1bf0e5f5ff3cfda327a
kei-kusanagi <kei_kusanagi@outlook.com>
2022-07-04 18:59:45 -0500

    edit content

--- django_tailwind_todo/todo/views.py
+++ django_tailwind_todo/todo/views.py
@@ -6,7 +6,20 @@ from .models import Todo
 
 def todos(request):
     todos = Todo.objects.all()
+
     return render(request, 'todo/todos.html', {'todos': todos})
+
+@require_http_methods(['GET', 'POST'])
+def edit_todo(request, pk):
+    todo = Todo.objects.get(pk=pk)
+
+    if request.method == 'POST':
+        todo.title = request.POST.get('title', '')
+        todo.save()
+
+        return render(request, 'todo/partials/todo.html', {'todo': todo})
+    
+    return render(request, 'todo/partials/edit.html', {'todo': todo})
 
 @require_http_methods(['POST'])
 def add_todo(request):
@@ -15,7 +28,7 @@ def add_todo(request):
 
     if title:
         todo = Todo.objects.create(title=title)
-
+    
     return render(request, 'todo/partials/todo.html', {'todo': todo})
 
 @require_http_methods(['PUT'])
@@ -23,10 +36,10 @@ def update_todo(request, pk):
     todo = Todo.objects.get(pk=pk)
     todo.is_done = True
     todo.save()
-    
+
     return render(request, 'todo/partials/todo.html', {'todo': todo})
 
-@require_http_methods('DELETE')
+@require_http_methods(['DELETE'])
 def delete_todo(request, pk):
     todo = Todo.objects.get(pk=pk)
     todo.delete()
